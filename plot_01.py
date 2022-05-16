12af539864f03a4f296421dd1079e403bceb5dc5
Jonathansartorib <jonathansartorib@hotmail.com>
2022-05-16 12:44:52 -0300

    Atualizacao csv

--- plot_01.py
+++ plot_01.py
@@ -1,18 +1,18 @@
 import plotly.graph_objects as go 
 import pandas as pd 
 
-df = pd.read_csv('https://raw.githubusercontent.com/ismarfrango/visualizacaoCS/master/southAmerica-pop.csv')
+df = pd.read_csv('https://raw.github.com/jonathansartorib/projeto_visualizacao_da_informacao/blob/main/console.csv')
 
 fig = go.Figure(data=go.Choropleth(
-    locations=df['name'], # Nome do país
-    z = df['pop'].astype(int), # Dados para o Choropleth
+    locations=df['Console_Name'], # Nome do país
+    z = df['Vendas'].astype(int), # Dados para o Choropleth
     locationmode = 'country names', # Tipo de identificção geográfica
     colorscale = 'Reds', #escala contínua em tons de vermelho
     colorbar_title = "População",
 ))
                               
 fig.update_layout(
-    title_text = 'População da América do Sul - 2019',     
+    title_text = 'Vendas de Consoles',     
     geo_scope='south america', # Limita escopo para a América do Sul 
 )  
 
